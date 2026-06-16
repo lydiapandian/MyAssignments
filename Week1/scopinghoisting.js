4d3b5e7a8bcdf9120eb8a949440d4e4a7af30912
lydiapandian <lydiapandian10@gmail.com>
2026-06-16 17:21:06 +0530

    Delete Week1 directory

--- Week1/scopinghoisting.js
+++
@@ -1,9 +0,0 @@
-const browserVersion= "Chrome"
-function getBrowserVersion(){
-if(browserVersion=="Chrome"){
- let browserVersion="Chrome";
-console.log("browserVersion");
-}
-console.log("browserVersion not found");
-}
-getBrowserVersion();
