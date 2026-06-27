ca87e4d04f6cb25e172f3a1067a590aae866f033
lydiapandian <lydiapandian10@gmail.com>
2026-06-27 20:15:07 +0530

    Delete findnum.js

--- findnum.js
+++
@@ -1,9 +0,0 @@
-function findsum(){
-    let numbers=[1,2,3,4,5]
-    let sum=0
-    for(let i=0;i<numbers.length;i++){
-        sum=sum+numbers[i]
-    }
-    console.log("sum of the number=",sum)
-}
-findsum();
